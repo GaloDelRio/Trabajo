9beb95f2f3582b4c22a159403e1b1bf492d555bf
GaloDelRio <111369089+GaloDelRio@users.noreply.github.com>
2022-09-23 21:15:35 -0500

    Update Avanve del Proyecto 3.py

--- Avanve del Proyecto 3.py
+++ Avanve del Proyecto 3.py
@@ -1,19 +1,69 @@
-print("ingresa el ingreso mensual")
-a=float(input())
 
-print("ingresa gastos contantes o servicios del mes")
-b=float(input())
+from ast import If
 
-print("ingresa gastos adicionales promedio")
-c=float(input())
 
 iva=.76
+i=0
 
-def gastos_del_mes(a,b,c):
-    operacion=a-(b*iva)-c
+def pesos(ingreso_mensual, gastos_constante,gastos_adicionales):
+    operacion=ingreso_mensual-(gastos_constante*iva)-gastos_adicionales
     if operacion > 0:
-        return print ("Tienes",operacion, "pesos, te gustaria invertir en NFTEC?")
+        print ("Tienes",operacion, "pesos, te gustaria invertir en NFTEC?")
     else:
-        return print (("Tienes",operacion,"pesos, eres pobre bienvenido a LATAM"))
+        print (("Tienes",operacion,"pesos, eres pobre bienvenido a LATAM"))
 
-gastos_del_mes(a,b,c)
+        
+
+def dolares(ingreso_mensual, gastos_constante,gastos_adicionales):
+    operacion=ingreso_mensual-(gastos_constante*iva)-gastos_adicionales
+    if operacion > 0:
+        print ("Tienes",operacion, "pesos, te gustaria invertir en NFTEC?")
+    else:
+        print (("Tienes",operacion,"pesos, eres pobre bienvenido a LATAM"))
+
+def agregar_dinero():
+    operación2=ingreso_mensual-(gastos_constante*iva)-gastos_adicionales
+    while i==0:
+        pregunta=str(input("¿Te gustaría agregar dinero?   y/n"))
+        if pregunta=="y":
+            preguta2=float(input("¿Cuanto dinero te gustaría agregar?"))
+            operación2=operación2+preguta2
+            print(operación2)
+        if pregunta=="n":
+            print("Gracias por utilizar la aplicación :)")
+            break
+
+
+
+opcion = str(input("¿Quieres usar dolares o pesos?"))
+if(opcion == "pesos"):
+    ingreso_mensual = float(input("ingresa el ingreso mensual"))
+    gastos_constante = float(input("ingresa gastos contantes o servicios del mes"))
+    gastos_adicionales = float(input("ingresa gastos adicionales promedio"))
+    pesos(ingreso_mensual, gastos_constante,gastos_adicionales)
+    operación2=ingreso_mensual-(gastos_constante*iva)-gastos_adicionales
+    while i==0:
+        pregunta=str(input("¿Te gustaría agregar dinero?   y/n"))
+        if pregunta=="y":
+            preguta2=float(input("¿Cuanto dinero te gustaría agregar?"))
+            operación2=operación2+preguta2
+            print(operación2)
+        if pregunta=="n":
+            print("Gracias por utilizar la aplicación :)")
+            break
+
+elif (opcion == "dolares"):
+    ingreso_mensual = int(input("ingresa el ingreso mensual"))
+    gastos_constante = int(input("ingresa gastos contantes o servicios del mes"))
+    gastos_adicionales = int(input("ingresa gastos adicionales promedio"))
+    dolares(ingreso_mensual, gastos_constante,gastos_adicionales)
+    operación2=ingreso_mensual-(gastos_constante*iva)-gastos_adicionales
+    while i==0:
+        pregunta=str(input("¿Te gustaría agregar dinero?   y/n"))
+        if pregunta=="y":
+            preguta2=float(input("¿Cuanto dinero te gustaría agregar?"))
+            operación2=operación2+preguta2
+            print(operación2)
+        if pregunta=="n":
+            print("Gracias por utilizar la aplicación :)")
+            break
